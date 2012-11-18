43e9e98b92e81815ae55da9335937bea48a62021
Arbind <arbind@rylyz.com>
2012-11-18 02:58:13 -0800

    spec: geo_cache

--- spec/lib/mixins/geo_aliases_spec.rb
+++ spec/lib/mixins/geo_aliases_spec.rb
@@ -26,7 +26,9 @@ describe :GeoAliases do
     @host = GeoAliasesHost.new
   end
 
-  it :caches_geocoded_url
+  it :geo_cache_is_empty do
+    REDIS.keys.should be_empty
+  end
 
   it :@geocode_address_before_save do
     # '3rd Street Promenade, Santa Monica CA' -> [lat:34.0169509, lng:-118.4977229]
@@ -42,6 +44,10 @@ describe :GeoAliases do
     geo_point = @host.geo_point
     @host.lat.should eq geo_point[:lat]     # +++ TODO break out geo point testing into separate spec
     @host.lng.should eq geo_point[:lng]     # +++ TODO break out geo point testing into separate spec
+  end
+
+  it :geo_cache_has_1 do
+    REDIS.keys.should have(1).things
   end
 
   it :@geocode_location_hash_before_save do
@@ -65,6 +71,11 @@ describe :GeoAliases do
     @host.lat.should be_between(38, 41)     # Lake Tahoe lat is about 39.1844571
     @host.lng.should be_between(-121, -118) # Lake Tahoe lng is about -120.1227438
   end
+
+  it :geo_cache_has_2 do
+    REDIS.keys.should have(2).things
+  end
+
 
   it :@reverse_geocode_before_save do
     # [lat: 39.1844571, lng:-120.1227438] -> 
